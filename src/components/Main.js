ce6c8c9a8e4dde93c333d8b441687d786ab3d47f
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-01-17 13:42:57 -0500

    add main

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import CardProduct from "./CardProduct";
+import Details from "./Details";
+
+const Main = () => {
+  return (
+        <div className="container grid grid-cols-2 mt-20 m-[auto] w-5/6 ">
+            <CardProduct/>
+            <Details/>
+        </div>
+  );
+};
+
+export default Main;
