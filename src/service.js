d6e00ef9201fd48bf2e8ca56a1a140105973ad43
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-01-17 13:40:57 -0500

    add array img products

---
+++ src/service.js
@@ -0,0 +1,22 @@
+export const arrImg = [
+  {
+    id: 1,
+    img: "./images/image-product-1.jpg",
+    card: "./images/image-product-1-thumbnail.jpg"
+  },
+  {
+    id: 2,
+    img: "./images/image-product-2.jpg",
+    card: "./images/image-product-2-thumbnail.jpg"
+  },
+  {
+    id: 3,
+    img: "./images/image-product-3.jpg",
+    card: "./images/image-product-3-thumbnail.jpg"
+  },
+  {
+    id: 4,
+    img: "./images/image-product-4.jpg",
+    card: "./images/image-product-4-thumbnail.jpg"
+  }
+];
