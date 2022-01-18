eec06b5219d861a60188d913a0c12bc3d3d66752
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-01-18 17:54:57 -0500

    carousel modal next and prev

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ const Main = () => {
   return (
         <div className="container grid grid-cols-2 mt-20 m-[auto] w-5/6 ">
             <CardProduct/>
-            <Details/>
+            <Details />
         </div>
   );
 };
