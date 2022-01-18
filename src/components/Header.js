fc1f003c90312d1435e0bd33aefc84ed89c7eda0
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-01-18 17:53:54 -0500

    styles and card count

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
+import { useContext } from "react";
+import { contextCount } from "../context/CountProvider";
 import Navbar from "./Navbar";
 
 const Header = () => {
+  const { state } = useContext(contextCount);
   return (
     <header >
       <div style={{ width: "80%", margin: "auto" }} className='flex items-center justify-between border-b py-8'>
@@ -9,7 +12,13 @@ const Header = () => {
            <Navbar />
         </div>
        <div className="flex w-32 justify-between items-center">
-          <img style={{ width: "20px", height: "20px" }} src="./images/icon-cart.svg" alt="cart" />
+         <div className="w-[25px] h-[25px] relative">
+           {
+              state.count > 0 &&
+              <span className="absolute right-[-5px] top-[-10px] bg-orange text-[10px] px-2 rounded-lg text-white font-bold">1</span>
+           }
+           <img className="w-full" src="./images/icon-cart.svg" alt="cart" />
+         </div>
           <img style={{ width: "40px" }} src="./images/image-avatar.png" alt="avatar" />
         </div>
       </div>
