702683bb4b2b14b0047094a9b2639873b4de1fad
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-01-18 17:53:25 -0500

    change width the carditem modal

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,8 +1,8 @@
-const CardItem = ({ handleItemActive, item, itemActiveId }) => {
+const CardItem = ({ handleItemActive, item, itemActive, width }) => {
   return (
-        <div style={{ width: "89px" }}
+        <div style={{ width: width || "89px" }}
           onClick={() => handleItemActive(item.id)}
-          className={`m-2 rounded-lg overflow-hidden cursor-pointer border ${itemActiveId === item.id && "active"}`}>
+          className={`m-2 rounded-lg overflow-hidden cursor-pointer border ${itemActive.id === item.id && "active"}`}>
             <img className="w-full" src={item.srcImgSmall} alt="card" />
         </div>
   );
