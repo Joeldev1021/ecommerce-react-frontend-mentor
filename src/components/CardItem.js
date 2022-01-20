3bf80b07c5c8153276857809eb9ebe22a27d33f8
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-01-20 09:33:52 -0500

    class responsive

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -2,7 +2,7 @@ const CardItem = ({ handleItemActive, item, itemActive, width }) => {
   return (
         <div style={{ width: width || "89px" }}
           onClick={() => handleItemActive(item.id)}
-          className={`m-2 rounded-lg overflow-hidden cursor-pointer border ${itemActive.id === item.id && "active"}`}>
+          className={`hidden md:flex  m-2 rounded-lg overflow-hidden hover-item cursor-pointer border ${itemActive.id === item.id && "active"}`}>
             <img className="w-full" src={item.srcImgSmall} alt="card" />
         </div>
   );
