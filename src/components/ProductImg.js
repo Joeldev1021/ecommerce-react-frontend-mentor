fd769a10ea0a99533002176d7b54bb46c0bc2220
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-01-20 09:29:43 -0500

    productImg with button next and prev

---
+++ src/components/ProductImg.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import ArrowIcon from "./ArrowIcon";
+import { arrImg } from "../service";
+
+const ProductImg = ({ itemActive }) => {
+  const [item, setItem] = useState(itemActive);
+
+  const handleItemActive = (n) => {
+    if (n === "next") {
+      const next = item.id + 1 > arrImg.length ? 1 : item.id + 1;
+      setItem(arrImg.find(i => i.id === next));
+    } else {
+      const prev = item.id - 1 < 1 ? arrImg.length : item.id - 1;
+      setItem(arrImg.find(i => i.id === prev));
+    }
+  };
+
+  return (
+    <>
+      <button onClick={() => handleItemActive("next")} className="absolute m-auto top-0 bottom-0 right-7  md:hidden w-[2.5rem] h-[2.5rem]  rounded-[50%] bg-gray-50 flex justify-center items-center">
+        <ArrowIcon arrow="next" />
+      </button>
+      <img className="w-[100%]" src={item.srcImg} alt="product-1" />
+      <button onClick={() => handleItemActive("prev")} className="absolute m-auto top-0 bottom-0 left-7 md:hidden w-[2.5rem] h-[2.5rem] rounded-[50%] bg-gray-50 flex justify-center items-center">
+        <ArrowIcon arrow="prev" />
+      </button>
+    </>
+  );
+};
+
+export default ProductImg;
