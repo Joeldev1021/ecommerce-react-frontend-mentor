a8e23e060fdbb7ac1265a9e78757099d22c0df31
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-01-20 09:31:06 -0500

    styles responsive grid col 1

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import Details from "./Details";
 
 const Main = () => {
   return (
-        <div className="container grid grid-cols-2 mt-20 m-[auto] w-5/6 ">
+        <div className="container grid grid-cols-1 md:grid-cols-2  md:mt-20 m-[auto] md:w-5/6 ">
             <CardProduct/>
             <Details />
         </div>
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -29,7 +29,7 @@ const Modal = ({ itemActive, handleModal }) => {
         <button onClick={() => handleModal()} className="absolute right-[5rem] text-white hover:text-orange">
           <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg">
             <path
-              className="fill-current "
+              className="fill-current"
               d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
               fillRule="evenodd"
             />
